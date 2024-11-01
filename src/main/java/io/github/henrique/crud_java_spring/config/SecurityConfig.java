ee0a3cd11905d483bda39109f1b444172203d2d7
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-01 10:25:43 -0300

    Configuring access credentials. Using UserDetails, PasswordEncoder and InMemoryUserDetailsManager to create temporary users in memory for testing.

--- src/main/java/io/github/henrique/crud_java_spring/config/SecurityConfig.java
+++ src/main/java/io/github/henrique/crud_java_spring/config/SecurityConfig.java
@@ -5,29 +5,69 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration // Anota a classe como uma classe de configuração do Spring
-@EnableWebSecurity // Habilita a segurança da web para o aplicativo
+@Configuration // Marca a classe como uma fonte de configurações do Spring
+@EnableWebSecurity // Habilita a segurança da web no projeto
 public class SecurityConfig {
 
-    // Define um bean que configura a cadeia de filtros de segurança
+    // Define um bean para a cadeia de filtros de segurança, que gerencia a configuração de segurança HTTP
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        // Configura a segurança HTTP
+        // Configura a segurança HTTP, aplicando regras de autorização
         return http
-                // Define as regras de autorização para requisições HTTP
+                // Configura regras de autorização para diferentes requisições HTTP
                 .authorizeHttpRequests(customizer -> {
-                    // Permite acesso público à URL "/public" sem autenticação
+                    // Permite que requisições para "/public" sejam acessadas por qualquer pessoa, sem autenticação
                     customizer.requestMatchers("/public").permitAll();
-                    // Requer autenticação para todas as outras requisições
+                    // Exige autenticação para qualquer outra requisição que não seja "/public"
                     customizer.anyRequest().authenticated();
                 })
-                // Habilita a autenticação HTTP Basic com as configurações padrão
+                // Habilita a autenticação HTTP Basic, que é um metodo de autenticação simples
                 .httpBasic(Customizer.withDefaults())
-                // Habilita o formulário de login com as configurações padrão
+                // Habilita a autenticação por formulário com as configurações padrão,
+                // fornecendo uma página de login padrão
                 .formLogin(Customizer.withDefaults())
-                .build(); // Constrói a configuração de segurança
+                .build(); // Constrói a configuração de segurança com todas as regras definidas
+    }
+
+    // Define um bean para o serviço de autenticação de usuários, que carrega e gerencia os detalhes dos usuários
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        // Cria um usuário comum com o nome "user", senha "123" (criptografada), e a role "USER"
+        UserDetails commonUser = User
+                .builder()
+                .username("user") // Define o nome de usuário
+                .password(passwordEncoder().encode("123")) // Define a senha criptografada
+                .roles("USER") // Define o papel (role) como "USER"
+                .build(); // Constrói o objeto UserDetails
+
+        // Cria um usuário administrador com o nome "admin", senha "admin" (criptografada),
+        // e as roles "USER" e "ADMIN"
+        UserDetails adminUser = User
+                .builder()
+                .username("admin") // Define o nome de usuário
+                .password(passwordEncoder().encode("admin")) // Define a senha criptografada
+                .roles("USER", "ADMIN") // Define os papéis (roles) como "USER" e "ADMIN"
+                .build(); // Constrói o objeto UserDetails
+
+        // Retorna um gerenciador de usuários em memória que usa os detalhes dos usuários criados para autenticação
+        return new InMemoryUserDetailsManager(commonUser, adminUser);
+    }
+
+    // Define um bean para o codificador de senhas, que é usado para criptografar senhas com o algoritmo BCrypt
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        // Retorna uma instância de BCryptPasswordEncoder, que aplica uma função de hash forte para
+        // proteger as senhas
+        return new BCryptPasswordEncoder();
     }
 
 }
