497b02eea4bb79f0a22e4cb0fa0faef837026925
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-31 23:31:05 -0300

    Started CRUD project with Java using SpringBoot 3 and Spring Security 6

---
+++ src/main/java/io/github/henrique/crud_java_spring/CrudJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.henrique.crud_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJavaSpringApplication.class, args);
+	}
+
+}
