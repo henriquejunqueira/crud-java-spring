bcf55fed154164b5f036a7792ef13ba3aa420bf9
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-02 10:01:25 -0300

    Creating the domain for the development of access control. User, Group and UserGroup entities created

---
+++ src/main/java/io/github/henrique/crud_java_spring/domain/entity/UsuarioGrupo.java
@@ -0,0 +1,22 @@
+package io.github.henrique.crud_java_spring.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class UsuarioGrupo { // essa classe faz o relacionamento entre as classes Usuário e Grupo
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_grupo")
+    private Grupo grupo;
+
+}
