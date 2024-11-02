2bd3633da3e09a285a4a0f40dacef8fe9a2f8e7e
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-02 11:25:32 -0300

    Encrypting the password and testing the user registration flow and permissions

--- src/main/java/io/github/henrique/crud_java_spring/domain/service/UsuarioService.java
+++ src/main/java/io/github/henrique/crud_java_spring/domain/service/UsuarioService.java
@@ -7,6 +7,7 @@ import io.github.henrique.crud_java_spring.domain.repository.GrupoRepository;
 import io.github.henrique.crud_java_spring.domain.repository.UsuarioGrupoRepository;
 import io.github.henrique.crud_java_spring.domain.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,10 +22,15 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final GrupoRepository grupoRepository;
     private final UsuarioGrupoRepository usuarioGrupoRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario salvar(Usuario usuario, List<String> grupos){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+
         usuarioRepository.save(usuario);
+
         List<UsuarioGrupo> listaUsuarioGrupo =grupos.stream().map(nomeGrupo -> {
            Optional<Grupo> possivelGrupo = grupoRepository.findByNome(nomeGrupo);
            if(possivelGrupo.isPresent()){
